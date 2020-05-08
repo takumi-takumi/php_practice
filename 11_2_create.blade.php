2bb265044e6222129312595c3ebf789f4ae6f325
takumi <takumiespanya@gmail.com>
2020-05-08 14:28:48 +0000

    【課題】PHP/Laravel 11 Bladeテンプレートの継承とLaravel Mixの使い方を活用してみよう　再

---
+++ 11_2_create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.profile')
+
+@section('title', 'プロフィールのページ')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィールのページ</h2>
+            </div>
+        </div>
+    </div>
+@endsection
