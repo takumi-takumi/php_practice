a08e659b0c5dfd903b692dc074f0ec02d8c02f27
takumi <takumiespanya@gmail.com>
2020-05-08 11:54:29 +0000

    【課題】PHP/Laravel 10 ControllerとViewが連携できるようにしよう

--- 10_1_create.blade.php
+++ 10_1_create.blade.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <title>課題1/2</title>
     </head>
     <body>
-        <h1>課題1/2 create</h1>
+        <h1>課題1/2</h1>
     </body>
 </html>
--- 10_2_edit.blade.php
+++ 10_2_edit.blade.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <title>課題2/2</title>
     </head>
     <body>
-        <h1>課題2/2 edit</h1>
+        <h1>課題2/2</h1>
     </body>
 </html>
