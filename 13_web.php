dd225fe586c26d1f410e837b2eb9985a62fb8826
takumi <takumiespanya@gmail.com>
2020-05-09 19:12:49 +0000

    【課題】PHP/Laravel 14 投稿データを保存しよう

--- 13_web.php
+++ 13_web.php
@@ -19,25 +19,13 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
     
-    //課題３ 
-    // Route::get('XXX','Admin\AAAController@bbb');
-    // 課題４
+    
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
-
-// Route::group(['prefix' => 'admin',], function() {
-//     Route::get('news/create', 'Admin\NewsController@add') ->middleware('auth');
-    
-//     //課題３ 
-//     // Route::get('XXX','Admin\AAAController@bbb');
-//     // 課題４
-//     Route::get('profile/create', 'Admin\ProfileController@add') ->middleware('auth');
-//     Route::get('profile/edit', 'Admin\ProfileController@edit') ->middleware('auth');  
-// });
 
 
 Auth::routes();
---
+++ 14_1_create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}" style="width:200px">
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10" style="display:flex">
+                            <input type="radio" class="form-control" name="gender" value="male" style="width:30px;height:23px"><p style="width:30px">男</p>
+                            <input type="radio" class="form-control" name="gender" value="female" style="width:30px;height:23px"><p style="width:30px">女</p>
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row" >
+                        <label class="col-md-2" for="introduction">自己紹介欄</label>
+                        <div class="col-md-10" style="position:relative">
+                            <textarea class="form-control" name="introduction" rows="15">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新" style="width:60px;height:40px;position:absolute;left:19%;top:100%;">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
