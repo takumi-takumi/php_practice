40879f61d1a45176a2fa208cd05e78397718e428
takumi <takumiespanya@gmail.com>
2020-05-11 03:10:14 +0000

    【課題】PHP/Laravel 17 編集履歴を実装しよう

---
+++ 17_ProfileController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Profile;
+use App\ProfileHistory;
+use Carbon\Carbon;
+
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        
+        $profiles = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $profiles->fill($form);
+        $profiles->save();
+        
+        return redirect('admin/profile');
+    }
+
+    public function index(Request $request)
+    {
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            $posts = Profile::where('name', $cond_name)->get();
+        } else {
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+    }
+
+
+    public function edit(Request $request)
+    {
+        $profiles = Profile::find($request->id);
+        if (empty($profiles)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profiles_form' => $profiles]);
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profiles = Profile::find($request->id);
+        $profiles_form = $request->all();
+        
+        unset($profiles_form['_token']);
+        
+        $profiles->fill($profiles_form)->save();
+        
+        $profilehistory = new ProfileHistory;
+        $profilehistory->profile_id = $profiles->id;
+        $profilehistory->edited_at = Carbon::now();
+        $profilehistory->save();
+        
+        return redirect('admin/profile/');
+    }
+
+
+    public function delete(Request $request)
+    {
+        $profiles = Profile::find($request->id);
+        $profiles->delete();
+    
+        return redirect('admin/profile/');
+    }
+
+}
+
+
+
---
+++ 17_edit.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.profile')
+@section('title', 'プロフィールの編集')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profiles_form->name }}" style="width:200px">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10" style="display:flex">
+                            <input type="radio" class="form-control" name="gender" value="male" style="width:30px;height:23px"><p style="width:30px">男</p>
+                            <input type="radio" class="form-control" name="gender" value="female" style="width:30px;height:23px"><p style="width:30px">女</p>
+                        </div>
+                    </div>
+                    <div class="for-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profiles_form->hobby }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介欄</label>
+                        <div class="col-md-10" style="position:relative">
+                            <textarea class="form-control" name="introduction" rows="15">{{ $profiles_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $profiles_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
+                </form>
+                <div class="row mt-5">
+                    <div class="col-md-4 mx-auto">
+                        <h2>編集履歴</h2>
+                        <ul class="list-group">
+                            @if ($profiles_form->profilehistories != NULL)
+                                @foreach ($profiles_form->profilehistories as $profilehistory)
+                                    <li class="list-group-item" style="width:180px">{{ $profilehistory->edited_at }}</li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
